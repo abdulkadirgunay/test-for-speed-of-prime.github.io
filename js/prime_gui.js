7e69fcb64786cfeaa20b0dbe00b4fa06fa9633a9
Andrej A Antonov <polymorphm@gmail.com>
2013-07-16 04:14:34 +0400

    first adding files

---
+++ js/prime_gui.js
@@ -0,0 +1,211 @@
+// -*- mode: js; coding: utf-8 -*-
+
+(function (global) {
+    'use strict'
+    
+    var module = global.app__prime_gui = {
+            async_prompt: function (text, val, callback) {
+                setTimeout(function () {
+                    try {
+                        var result = prompt(text, val)
+                        
+                        callback(null, result)
+                    } catch (err) {
+                        callback(err, null)
+                    }
+                }, 0)
+            },
+            
+            async_alert: function (text) {
+                setTimeout(function () {
+                    alert(text)
+                }, 0)
+            },
+            
+            html_escape: function (val) {
+                var elem = document.createElement('span')
+                
+                elem.textContent = val
+                return elem.innerHTML
+            },
+            
+            html_unescape: function (val) {
+                var elem = document.createElement('span')
+                
+                elem.innerHTML = val
+                return elem.textContent
+            },
+            
+            TestCtx: function () {},
+            
+            write_to_log: function (test_ctx, text) {
+                test_ctx.log_elem.innerHTML += module.html_escape(text) + '\n'
+            },
+            
+            write_to_log_with_bold: function (test_ctx, text) {
+                test_ctx.log_elem.innerHTML += '<b>' + module.html_escape(text) + '</b>\n'
+            },
+            
+            write_to_log_with_date: function (test_ctx, text) {
+                test_ctx.log_elem.innerHTML +=
+                        '<b>' + module.html_escape(new Date().toLocaleString()) + '</b>: ' +
+                        module.html_escape(text) + '\n'
+            },
+            
+            on_prime_worker_msg: function (test_ctx, data) {
+                if (test_ctx.is_closed || !test_ctx.is_busy) {
+                    return
+                }
+                
+                if (data.done) {
+                    test_ctx.is_busy = false
+                    test_ctx.end_time = Date.now()
+                    var work_time = test_ctx.end_time - test_ctx.begin_time
+                    
+                    module.write_to_log_with_date(
+                            test_ctx,
+                            'end test (work time is ' +work_time + 'ms)'
+                            )
+                    
+                    return
+                }
+                
+                module.write_to_log_with_date(
+                                test_ctx,
+                                'found prime (' + data.prime + ')'
+                                )
+            },
+            
+            run_test: function (test_ctx) {
+                if (test_ctx.is_closed) {
+                    return
+                }
+                
+                if (test_ctx.is_busy) {
+                    module.async_alert('error: test already is running')
+                    return
+                }
+                
+                test_ctx.is_busy = true
+                
+                module.async_prompt('Prime From', '500000000', function (err, prime_from) {
+                    if (test_ctx.is_closed) {
+                        return
+                    }
+                    
+                    if (prime_from === null) {
+                        test_ctx.is_busy = false
+                        return
+                    }
+                    
+                    prime_from = new Number(prime_from)
+                    
+                    if (isNaN(prime_from) || prime_from <= 0) {
+                        module.async_alert('error: invalid value')
+                        test_ctx.is_busy = false
+                        return
+                    }
+                    
+                    module.async_prompt('Prime Count', '10', function (err, prime_count) {
+                        if (test_ctx.is_closed) {
+                            return
+                        }
+                        
+                        if (prime_count === null) {
+                            test_ctx.is_busy = false
+                            return
+                        }
+                        
+                        prime_count = new Number(prime_count)
+                        
+                        if (isNaN(prime_count) || prime_count <= 0) {
+                            module.async_alert('error: invalid value')
+                            test_ctx.is_busy = false
+                            return
+                        }
+                        
+                        test_ctx.prime_from = prime_from
+                        test_ctx.prime_count = prime_count
+                        test_ctx.begin_time = Date.now()
+                        
+                        module.write_to_log_with_date(
+                                test_ctx,
+                                'begin test (prime_from is ' + test_ctx.prime_from + '; ' +
+                                'prime_count is ' + test_ctx.prime_count + ')'
+                                )
+                        
+                        test_ctx.prime_worker.postMessage({
+                                version: test_ctx.version,
+                                prime_from: test_ctx.prime_from,
+                                prime_count: test_ctx.prime_count,
+                                })
+                    })
+                })
+            },
+            
+            init_log: function (test_ctx) {
+                module.write_to_log_with_bold(
+                        test_ctx,
+                        'Test Version: ' + test_ctx.version + '\n' +
+                        'Browser: ' + navigator.userAgent + '\n'
+                        )
+            },
+            
+            init_elem: function (elem, prime_worker_url) {
+                if (!elem) {
+                    return
+                }
+                
+                var run_test_button = document.createElement('a')
+                run_test_button.href = '#'
+                run_test_button.textContent = 'Run Test'
+                
+                var clear_log_button = document.createElement('a')
+                clear_log_button.href = '#'
+                clear_log_button.textContent = 'Clear Log'
+                
+                var buttons_block = document.createElement('div')
+                buttons_block.style.margin = '1em 0'
+                buttons_block.appendChild(run_test_button)
+                buttons_block.appendChild(document.createTextNode(' | '))
+                buttons_block.appendChild(clear_log_button)
+                
+                var log_elem = document.createElement('pre')
+                log_elem.style.margin = '1em 0'
+                log_elem.style.border = '1px solid'
+                log_elem.style.padding = '2em'
+                
+                var prime_worker = new Worker(prime_worker_url)
+                
+                var test_ctx = new module.TestCtx
+                test_ctx.version = '2013_07_15_12_04'
+                test_ctx.is_closed = false
+                test_ctx.is_busy = false
+                test_ctx.log_elem = log_elem
+                test_ctx.prime_worker = prime_worker
+                
+                prime_worker.addEventListener('message', function (evt) {
+                    module.on_prime_worker_msg(test_ctx, evt.data)
+                })
+                
+                run_test_button.addEventListener('click', function (evt) {
+                    evt.preventDefault()
+                    
+                    module.run_test(test_ctx)
+                })
+                
+                clear_log_button.addEventListener('click', function (evt) {
+                    evt.preventDefault()
+                    
+                    test_ctx.log_elem.innerHTML = ''
+                    module.init_log(test_ctx)
+                })
+                
+                module.init_log(test_ctx)
+                
+                elem.innerHTML = ''
+                elem.appendChild(buttons_block)
+                elem.appendChild(log_elem)
+            },
+            }
+})(this)
---
+++ js/prime_worker.js
@@ -0,0 +1,65 @@
+// -*- mode: js; coding: utf-8 -*-
+
+(function (global) {
+    'use strict'
+    
+    function create_asm_module (stdlib) {
+        'use asm'
+        
+        function find_prime (prime_from) {
+            prime_from = prime_from | 0
+            
+            var d = 0
+            
+            if (!((prime_from | 0) % 2)) {
+                prime_from = prime_from + 1 | 0
+            }
+            
+            for (;;) {
+                
+                for (d = 3; (d | 0) < (prime_from | 0); d = d + 2 | 0) {
+                    if (!((prime_from | 0) % (d | 0))) {
+                        break
+                    }
+                }
+                
+                if ((d | 0) >= (prime_from | 0)) {
+                    break
+                }
+                
+                prime_from = prime_from + 2 | 0
+            }
+            
+            return prime_from | 0
+        }
+        
+        return {
+            find_prime: find_prime,
+        }
+    }
+    
+    var asm_module = create_asm_module(global)
+    
+    global.onmessage = function (evt) {
+        if (evt.data.version != '2013_07_15_12_04') {
+            return
+        }
+        
+        var prime_from = evt.data.prime_from | 0
+        var prime_count = evt.data.prime_count | 0
+        
+        for (var prime_i = 0; prime_i < prime_count; ++prime_i) {
+            prime_from = asm_module.find_prime(prime_from)
+            
+            postMessage({
+                    prime: prime_from,
+                    })
+            
+            prime_from += 2
+        }
+        
+        postMessage({
+                done: true,
+                })
+    }
+})(this)
